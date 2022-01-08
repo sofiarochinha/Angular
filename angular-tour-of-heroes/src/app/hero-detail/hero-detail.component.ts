2e32fb0a5d875514191de2e6d20a8fb428be2d2b
Sofia <s.rocha@ua.pt>
2022-01-08 11:52:42 +0000

    Create components

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero?: Hero; //make the hero property available for binding by the external HeroesComponent
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
